37ec9d7af1006ec8b967de4598a14678d43cabeb
haoyanghy <aarontan113388@gmail.com>
2025-08-10 06:22:14 +0800

    remove unnecessary code

--- rofl-agent/config.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  sapphireRpcUrl: process.env.SAPPHIRE_RPC_URL || "https://testnet.sapphire.oasis.dev",
-  contractAddress: process.env.CONTRACT_ADDRESS || "0xYourContractAddress",
-  privateKey: process.env.PRIVATE_KEY || "0xYourPrivateKey",
-  mockApiPort: process.env.MOCK_API_PORT || 4000,
-};
